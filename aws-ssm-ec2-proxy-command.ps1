0eb63884ec7a332c217af170aaf49732c3816ece
Vibin Daniel <vibind@drivevariant.com>
2021-02-23 00:05:44 -0500

    Initial commit

---
+++ aws-ssm-ec2-proxy-command.ps1
@@ -0,0 +1,51 @@
+######## Source ################################################################
+# Open SSH Connection
+#   ssh <INSTANCE_USER>@<INSTANCE_ID>:<AWS_PROFILE>
+################################################################################
+$ErrorActionPreference = "stop"
+
+$AWS_PROFILE_SEPARATOR = ':'
+
+$ec2_instance_id = "$($args[0])"
+$ssh_user = "$($args[1])"
+$ssh_port = "$($args[2])"
+$ssh_public_key_path = "$($args[3])"
+$ssh_public_key = "$(Get-Content "${ssh_public_key_path}")" -replace '\\', '\\'
+
+if ($ec2_instance_id -match $AWS_PROFILE_SEPARATOR)
+{
+  $env:AWS_PROFILE = $ec2_instance_id.Split($AWS_PROFILE_SEPARATOR)[1]
+  $ec2_instance_id = $ec2_instance_id.Split($AWS_PROFILE_SEPARATOR)[0]
+}
+
+
+$ssm_command = @"
+{
+  "Parameters": {
+    "commands": [
+      "mkdir -p /home/${ssh_user}/.ssh",
+      "cd /home/${ssh_user}/.ssh || exit 1",
+      "authorized_key='${ssh_public_key} ssm-session'",
+      "echo `$authorized_key >> authorized_keys",
+      "sleep 24h",
+      "grep -v -F `$authorized_key authorized_keys > .authorized_keys",
+      "mv .authorized_keys authorized_keys"
+    ]
+  }
+}
+"@
+
+Set-Content -Path "./test.json" -Value $ssm_command
+
+Write-Host "Add public key ${ssh_public_key_path} to instance ${ec2_instance_id} for 24 hours"
+aws ssm send-command `
+  --instance-ids "${ec2_instance_id}" `
+  --document-name 'AWS-RunShellScript' `
+  --comment "Add an SSH public key to authorized_keys for 24 hours" `
+  --cli-input-json file://test.json
+
+Write-Host "Start ssm session to instance ${ec2_instance_id}"
+aws ssm start-session `
+  --target "${ec2_instance_id}" `
+  --document-name 'AWS-StartSSHSession' `
+  --parameters "portNumber=${ssh_port}"
---
+++ install.ps1
@@ -0,0 +1,78 @@
+$ErrorActionPreference = "stop"
+
+try
+{
+  aws --version
+}
+catch
+{
+  if ($PSVersionTable.OS -match "Windows")
+  {
+    Start-Process "https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-windows.html"
+  }
+  elseif ($PSVersionTable.OS -match "Linux")
+  {
+    Invoke-WebRequest "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+    Remove-Item awscliv2.zip
+    Remove-Item aws -Force -Recurse
+  }
+  elseif ($PSVersionTable.OS -match "Darwin")
+  {
+    Invoke-WebRequest "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+    sudo installer -pkg AWSCLIV2.pkg -target /
+    Remove-Item AWSCLIV2.pkg
+  }
+  aws --version
+}
+
+try
+{
+  session-manager-plugin
+}
+catch
+{
+  if ($PSVersionTable.OS -match "Windows")
+  {
+    Start-Process "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html#install-plugin-windows"
+  }
+  elseif ($PSVersionTable.OS -match "Linux")
+  {
+    Invoke-WebRequest "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+    sudo dpkg -i session-manager-plugin.deb
+    Remove-Item session-manager-plugin.deb
+  }
+  elseif ($PSVersionTable.OS -match "Darwin")
+  {
+    Invoke-WebRequest "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip" -o "sessionmanager-bundle.zip"
+    unzip sessionmanager-bundle.zip
+    sudo ./sessionmanager-bundle/install -i /usr/local/sessionmanagerplugin -b /usr/local/bin/session-manager-plugin
+    Remove-Item sessionmanager-bundle.zip
+  }
+  session-manager-plugin
+}
+
+New-Item -ItemType Directory -Force -Path "$HOME/.ssh/config"
+Copy-Item aws-ssm-ec2-proxy-command.sh "$HOME/.ssh/aws-ssm-ec2-proxy-command.ps1"
+chmod +x "$HOME/.ssh/aws-ssm-ec2-proxy-command.ps1"
+
+if (! $(Get-Content "$HOME/.ssh/config") -match "host i-* mi-*")
+{
+  if ($PSVersionTable.OS -match "Windows")
+  {
+    $pwsh = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+  }
+  elseif ($PSVersionTable.OS -match "Linux" -or $PSVersionTable.OS -match "Darwin")
+  {
+    $pwsh = $(which pwsh)
+  }
+  $text = @"
+
+host i-* mi-*
+  IdentityFile $HOME/.ssh/id_rsa
+  ProxyCommand $pwsh "$HOME/.ssh/aws-ssm-ec2-proxy-command.ps1 %h %r %p $HOME/.ssh/id_rsa.pub"
+  StrictHostKeyChecking no
+"@
+  Add-Content -Path "$HOME/.ssh/config" -Value $text
+}
