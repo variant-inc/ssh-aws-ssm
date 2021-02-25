760930c8c51e36adf980ec628354ccf7210f548a
Vibin Daniel <vibind@drivevariant.com>
2021-02-25 09:52:41 -0500

    Run chmod +x only for linux and mac

--- install.ps1
+++ install.ps1
@@ -57,7 +57,6 @@ catch
 
 New-Item -ItemType Directory -Force -Path "$HOME/.ssh"
 Copy-Item aws-ssm-ec2-proxy-command.sh "$HOME/.ssh/aws-ssm-ec2-proxy-command.ps1"
-chmod +x "$HOME/.ssh/aws-ssm-ec2-proxy-command.ps1"
 
 if (!(Test-Path "$HOME/.ssh/config"))
 {
@@ -73,6 +72,7 @@ if (! $(Get-Content "$HOME/.ssh/config") -match "host i-* mi-*")
   }
   elseif ($PSVersionTable.OS -match "Linux" -or $PSVersionTable.OS -match "Darwin")
   {
+    chmod +x "$HOME/.ssh/aws-ssm-ec2-proxy-command.ps1"
     $pwsh = $(which pwsh)
   }
   $text = @"
